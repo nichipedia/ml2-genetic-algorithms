9c528eca9ab8fda44e03098993a078a3660afe01
Nicholas <atreayou@gmail.com>
2018-02-11 20:13:55 -0600

    Switched the boolean on amino acid to final.

--- src/main/java/com/realgood/ml2program1/models/AminoAcid.java
+++ src/main/java/com/realgood/ml2program1/models/AminoAcid.java
@@ -4,7 +4,7 @@ package com.realgood.ml2program1.models;
  * Created by NicholasMoran on 2/11/18.
  */
 public class AminoAcid {
-    private boolean hydrophobic;
+    private final boolean hydrophobic;
 
     public AminoAcid(boolean hydrophobic) {
         this.hydrophobic = hydrophobic;
