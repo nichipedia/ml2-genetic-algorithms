40a14e0f249fbc93fe78f4d06298281257d32a8c
nichipedia <atreayou@gmail.com>
2018-02-23 15:26:08 -0600

    added comments

--- src/main/java/com/realgood/ml2program1/models/PSNode.java
+++ src/main/java/com/realgood/ml2program1/models/PSNode.java
@@ -1,16 +1,24 @@
 package com.realgood.ml2program1.models;
 
 import com.realgood.ml2program1.enums.Vector;
-
-/**
- * Created by NicholasMoran on 2/14/18.
- */
+/*******************************************
+ * <h1>PSNode</h1>                          *
+ * Model representation of a point on a graph
+ * @author Nicholas Moran                   *
+ * @version 1.0                             *
+ *                                          *
+ *******************************************/
 public class PSNode {
     private final Point point;
     private final AminoAcid acid;
     private final int step;
 
-
+    /***
+     * Constructor for a PSNode
+     * @param point
+     * @param acid
+     * @param step
+     */
     public PSNode(Point point, AminoAcid acid, int step) {
         this.point = point;
         this.acid = acid;
